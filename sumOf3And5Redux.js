85feae6a10ad6753c4139a9a84b7b0c65f082750
SanMasood <sanamasood.giki@gmail.com>
2020-06-09 00:59:56 +0100

    Code returns sum of multiples of 3 and 5

---
+++ sumOf3And5Redux.js
@@ -0,0 +1,19 @@
+function myFunc(int){  
+    
+    let arr1 =[];
+    let i;
+    for (let i = (int-1); i>0 ; i--){
+
+        if (i%3===0 || i%5===0)
+        arr1.push(i);
+    } 
+    
+  
+    let unique = arr1.filter((v, i, a) => a.indexOf(v) === i)
+    .reduce((sum, acc)=> sum+acc, 0); 
+    console.log(unique);    
+
+    //return (unique);
+  }
+
+  myFunc(88);
